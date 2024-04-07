2d7dd1b7d79eb79f6ef5eaf8ccb20fecdff693fd
Matthias Lopinski <70134088+MatzeLopi@users.noreply.github.com>
2024-04-07 19:16:30 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,54 +1,72 @@
-// Create a WebServer
-// Use express
-// Use body-parser
+// Create web server
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+const url = require('url');
+const { getComments, addComment } = require('./comments');
 
-// Require express
-var express = require('express');
-// Require body-parser
-var bodyParser = require('body-parser');
-
-// Create an instance of express
-var app = express();
-
-// Use body-parser
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
-
-// Create a comments array
-var comments = [
-  'First comment',
-  'Second comment',
-  'Third comment'
-];
-
-// Create a get route
-app.get('/comments', function(req, res) {
-  res.send(comments);
+const server = http.createServer((req, res) => {
+  const parsedUrl = url.parse(req.url, true);
+  const { pathname } = parsedUrl;
+  if (req.method === 'GET' && pathname === '/comments') {
+    const comments = getComments();
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(comments));
+  } else if (req.method === 'POST' && pathname === '/comments') {
+    let body = '';
+    req.on('data', (chunk) => {
+      body += chunk.toString();
+    });
+    req.on('end', () => {
+      const { comment } = JSON.parse(body);
+      addComment(comment);
+      res.end();
+    });
+  } else {
+    res.statusCode = 404;
+    res.end();
+  }
 });
 
-// Create a post route
-app.post('/comments', function(req, res) {
-  var comment = req.body.comment;
-  comments.push(comment);
-  res.send(comments);
+server.listen(3000, () => {
+  console.log('Server is listening on http://localhost:3000');
+});
+// Path: comments.js
+// Create web server
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+const url = require('url');
+const { getComments, addComment } = require('./comments');
+
+const server = http.createServer((req, res) => {
+  const parsedUrl = url.parse(req.url, true);
+  const { pathname } = parsedUrl;
+  if (req.method === 'GET' && pathname === '/comments') {
+    const comments = getComments();
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(comments));
+  } else if (req.method === 'POST' && pathname === '/comments') {
+    let body = '';
+    req.on('data', (chunk) => {
+      body += chunk.toString();
+    });
+    req.on('end', () => {
+      const { comment } = JSON.parse(body);
+      addComment(comment);
+      res.end();
+    });
+  } else {
+    res.statusCode = 404;
+    res.end();
+  }
 });
 
-// Create a put route
-app.put('/comments/:index', function(req, res) {
-  var index = req.params.index;
-  var comment = req.body.comment;
-  comments[index] = comment;
-  res.send(comments);
+server.listen(3000, () => {
+  console.log('Server is listening on http://localhost:3000');
 });
-
-// Create a delete route
-app.delete('/comments/:index', function(req, res) {
-  var index = req.params.index;
-  comments.splice(index, 1);
-  res.send(comments);
-});
-
-// Listen on port 3000
-app.listen(3000, function() {
-  console.log('Server is running on port 3000');
-});
+// Path: comments.js
+// Create web server
+const http = require('http');
+const fs = require('fs');
+const
